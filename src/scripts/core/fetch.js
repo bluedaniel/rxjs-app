4c3827e9eceb61e08dce6e4371c14182c2f21f02
bluedaniel <daniellevitt86@gmail.com>
2016-09-14 09:11:30 +0100

    More comments

--- src/scripts/core/fetch.js
+++ src/scripts/core/fetch.js
@@ -1,40 +1,31 @@
 import { Observable } from 'rxjs/Observable';
 import { globalStore } from 'stores/';
-import { identity, compose, filter, isEmpty, propEq, is, prop, length, warn } from 'core/utils';
+import { identity, compose, filter, isEmpty, propEq, is, length, warn } from 'core/utils';
 import 'whatwg-fetch';
-
-const postHeaders = { headers: { 'content-type': 'application/json' } };
-
-const check = response => {
-  const { status } = response;
-  if (status >= 200 && status < 300 || status === 403 || status === 401) return response;
-  throw response;
-};
 
 const f = method => (url, data = {}) =>
   fetch(url, {
     credentials: 'include',
     method,
-    ...(method === 'post' ? postHeaders : {}),
+    ...(method === 'post' ? { headers: { 'content-type': 'application/json' } } : {}),
     ...data,
     ...(data.body && is(Object, data.body) ? { body: JSON.stringify(data.body) } : {})
   })
-  .then(check)
-  .then(response => response.json())
-  .catch(e => {
-    console.log(e);
-    throw e;
-  });
+  .then(checkResponse)
+  .then(resp => resp.json())
+  .catch(e => { throw e; });
 
-const successStatus = compose(isEmpty, prop('errors'));
-const unauthorizedStatus = compose(length, filter(propEq('code', 215)), prop('errors'));
-const errorStatus = ({ errors }) => {
-  return !successStatus({ errors }) && !unauthorizedStatus({ errors });
+const checkResponse = resp => {
+  const { status } = resp;
+  if (status >= 200 && status < 300 || status === 403 || status === 401) return resp;
+  throw resp;
 };
 
+// Functions used around the app to fetch
 export const get = f('get');
 export const post = f('post');
 
+// Collection of observables to merge and emit request results
 export const request = (fnRequest, actions, {
   retry = 2,
   immediate = () => Observable.of(globalStore.toggleLoading(true, 'Fetching')),
@@ -47,6 +38,7 @@ export const request = (fnRequest, actions, {
 }) => {
   const request$ = Observable.defer(fnRequest)
     .retryWhen(errors$ =>
+      // Incremental back-off strategy
       Observable.zip(Observable.range(1, retry), errors$)
         .do(([ i, { response } ]) => warn(`Error fetching ${response}, retrying in ${i} second(s)`))
         .mergeMap(([ i, err ]) => {
@@ -59,15 +51,22 @@ export const request = (fnRequest, actions, {
     .catch(message => Observable.of({ errors: [ { message } ] }))
     .share();
 
-  const onImmediate$ = immediate();
+  // Filters for emitting the correct success/auth/error stream from request
   const onResponse$ = Observable.merge(
-    unauthorized(request$.filter(unauthorizedStatus)),
-    error(request$.filter(errorStatus)),
-    success(request$.filter(successStatus))
+    unauthorized(request$.filter(({ errors }) => unauthorizedStatus(errors))),
+    error(request$.filter(({ errors }) => errorStatus(errors))),
+    success(request$.filter(({ errors }) => successStatus(errors)))
   ).filter(identity).share();
+
+  const onImmediate$ = immediate();
 
   return onImmediate$ ? onImmediate$.merge(
     onImmediate$.mergeMap(() => onResponse$),
     onResponse$.mergeMap(always).filter(identity)
   ) : onResponse$;
 };
+
+// Functions to check if API response contains errors
+const successStatus = compose(isEmpty);
+const unauthorizedStatus = compose(length, filter(propEq('code', 215)));
+const errorStatus = (errors) => !successStatus(errors) && !unauthorizedStatus(errors);
