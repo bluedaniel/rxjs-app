8c7a4f53eba444e90e72781401bec64a22aa7b7a
bluedaniel <daniellevitt86@gmail.com>
2016-09-21 13:12:38 +0100

    Error & Toast specs

---
+++ test/stores/error.spec.js
@@ -0,0 +1,47 @@
+import { test } from 'ava';
+import { compose, view, lensPath, prop, head } from 'core/utils';
+import { errorStore } from '../../src/scripts/stores/error';
+
+const viewErrors = view(lensPath([ 'errorStore', 'errors' ]));
+const defaultState = { errorStore: errorStore.defaultState() };
+
+test('[core/stores/error] getTimeout', t => {
+  t.is(errorStore.getTimeout(), 4000);
+});
+
+test('[core/stores/error] addError', t => {
+  const newState = compose(
+    errorStore.addError({ message: '¯\_(ツ)_/¯' })
+  )(defaultState);
+
+  t.is(viewErrors(defaultState).length, 0);
+  t.is(viewErrors(newState).length, 1);
+  t.is(viewErrors(newState)[0].message, '¯\_(ツ)_/¯');
+});
+
+test('[core/stores/error] removeError', t => {
+  const newState = compose(
+    errorStore.addError({ message: 'err2' }),
+    errorStore.addError({ message: 'err1' })
+  )(defaultState);
+
+  const id = compose(prop('id'), head, viewErrors)(newState);
+  const finalState = errorStore.removeError(id)(newState);
+
+  t.is(viewErrors(finalState).length, 1);
+  t.is(viewErrors(finalState)[0].message, 'err2');
+});
+
+test('[core/stores/error] removeErrors', t => {
+  const time = Date.now() - (errorStore.getTimeout() * 1.1);
+  const newState = compose(
+    errorStore.addError({ time, message: 'err3' }),
+    errorStore.addError({ time, message: 'err2' }),
+    errorStore.addError({ time, message: 'err1' })
+  )(defaultState);
+
+  const finalState = errorStore.removeErrors()(newState);
+
+  t.is(viewErrors(newState).length, 3);
+  t.is(viewErrors(finalState).length, 0);
+});
---
+++ test/stores/toast.spec.js
@@ -0,0 +1,34 @@
+import { test } from 'ava';
+import { compose, view, lensPath } from 'core/utils';
+import { toastStore } from '../../src/scripts/stores/toast';
+
+const viewToasts = view(lensPath([ 'toastStore', 'toasts' ]));
+const defaultState = { toastStore: toastStore.defaultState() };
+
+test('[core/stores/toast] getTimeout', t => {
+  t.is(toastStore.getTimeout(), 5000);
+});
+
+test('[core/stores/toast] addToast', t => {
+  const newState = compose(
+    toastStore.addToast({ message: '¯\_(ツ)_/¯' })
+  )(defaultState);
+
+  t.is(viewToasts(defaultState).length, 0);
+  t.is(viewToasts(newState).length, 1);
+  t.is(viewToasts(newState)[0].message, '¯\_(ツ)_/¯');
+});
+
+test('[core/stores/toast] removeToasts', t => {
+  const time = Date.now() - (toastStore.getTimeout() * 1.1);
+  const newState = compose(
+    toastStore.addToast({ time, message: 'toasty3' }),
+    toastStore.addToast({ time, message: 'toasty2' }),
+    toastStore.addToast({ time, message: 'toasty1' })
+  )(defaultState);
+
+  const finalState = toastStore.removeToasts()(newState);
+
+  t.is(viewToasts(newState).length, 3);
+  t.is(viewToasts(finalState).length, 0);
+});
